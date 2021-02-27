fc7e939974568aa281315d332456279d8993d5bc
orvinrfc <orvinrfc@hotmail.com>
2021-02-26 19:54:54 -0800

    added findCompany, now user will be able to ask about the production companies of the movie

--- company.py
+++ company.py
@@ -1,3 +1,30 @@
 import imdb
 
 ia = imdb.IMDb()
+
+def findCompany(movie): # Find the companies that produce the given movie
+    # lookup for movie and get the movie id
+    moviesId = movie.movieID 
+
+    # getting information of the movie
+    movie = ia.get_movie(moviesId)
+
+    # getting the movie name
+    movieName = str(movie)
+
+    # get the list of companies of the movie 
+    companies = movie.data['production companies']
+
+    print("The production companies for the movie " + movieName + " are:")
+
+    # print the companies
+    for company in companies:
+        print(company['name'])
+
+    return companies # return the set of companies
+
+
+
+    
+
+
