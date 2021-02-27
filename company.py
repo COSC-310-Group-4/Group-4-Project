1c91218d5a89570eaeeb9c997c309ce27c4b148c
orvinrfc <orvinrfc@hotmail.com>
2021-02-27 13:19:27 -0800

    changes company.py now only display and return the first main production company

--- company.py
+++ company.py
@@ -3,26 +3,20 @@ import imdb
 ia = imdb.IMDb()
 
 def findCompany(movie): # Find the companies that produce the given movie
-    # lookup for movie and get the movie id
-    moviesId = movie.movieID 
-
-    # getting information of the movie
-    movie = ia.get_movie(moviesId)
-
     # getting the movie name
-    movieName = str(movie)
+    movieName = movie['title']
 
     # get the list of companies of the movie 
     companies = movie.data['production companies']
 
     if len(companies) == 0:
         print("IMDBot: There is no production company for this movie")
+        return;
     else:
-        print("IMDBot: The production companies for the movie " + movieName + " are:")
+        print("IMDBot: The production company is: " + companies[0]['name'])
 
-        # print the companies
-        for company in companies:
-            print(company['name'])
+    return companies[0] # return the main company of the movie production
+
+    
 
 
-    return companies # return the set of companies
